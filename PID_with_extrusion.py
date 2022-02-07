02a07f2f3e65a32f4dc7eea2869a37ca82c89e0d
Akash Gupta <ag2118@cam.ac.uk>
2022-02-07 12:58:08 +0000

    updated pid with extrusion

--- PID_with_extrusion.py
+++ PID_with_extrusion.py
@@ -32,6 +32,7 @@ max_timesteps = 30
 fig, ax = plt.subplots()
 line,= ax.plot([],[], label="Nozzle Temperature")
 target_line, = ax.plot([],[], label="Target Temperature")
+extrude_line, = ax.plot([],[], label="Feed Rate")
 ax.set_ylabel("Nozzle Temp")
 ax.set_xlabel('Time')
 ax.legend()
@@ -39,19 +40,46 @@ ax.legend()
 nozzle_temp_data = np.array([])
 time_data = np.array([])
 # target_data = np.array([])
+extrude_data = np.array([])
 
+# Set extrusion type as absolute
+ser.write(f"M83\r\n".encode())
+
+extrude_phase = 0
+def extrude_update():
+  """Function to send targets"""
+  global extrude_data
+  while True:
+    # Phase 0: Preheat nozzle to 200
+    if extrude_phase == 0:
+      ser.write(f"G1 F350 E0\r\n".encode())
+      yield 0
+      # extrude_data = np.append(extrude_data, [0])
+
+    # Phase 1: Send a step disturbance
+    if extrude_phase == 1:
+      feed_rate = 360
+      ser.write(f"G1 F{feed_rate} E50\r\n".encode())
+      yield feed_rate
+      # extrude_data = np.append(extrude_data, [feed_rate])
+extrude_update_gen = extrude_update()
 
 def update(i):
-  global nozzle_temp_data, time_data
+  global nozzle_temp_data, time_data, extrude_data
 
   # Read temperature measurement
   # NOTE: This measurement lags behind by 0.5s (i.e. update interval)
   nozzle_temp_data = np.append(nozzle_temp_data, [utils.extract_nozzle_temp(ser)])
   time_data = np.append(time_data, [len(time_data) * timestep])
+  extrude_data = np.append(extrude_data, [next(extrude_update_gen)])
   
+  # print(f"Extrude data", extrude_data)
+  # extrude_update()
+
   # Replot line
   line.set_data(time_data, nozzle_temp_data)
   target_line.set_data(time_data, [temp_target for _ in time_data])
+  extrude_line.set_data(time_data, extrude_data)
   ax.autoscale()
   ax.relim()
   ax.autoscale_view()
@@ -59,11 +87,10 @@ def update(i):
   # Send command to printer to measure temperature
   ser.write(b'M105\r\n')
     
-
 # Heat up nozzle to desired temperature
 # NOTE: MAY want to visualise this to check
 # if temperature has truly settled
-utils.set_nozzle_temp(ser, temp=temp_target, avg_num=10, tol=0.5)
+# utils.set_nozzle_temp(ser, temp=temp_target, avg_num=10, tol=0.5)
 
 # Set desired temperature
 ser.write(f"M104 S{temp_target}\r\n".encode())
@@ -73,6 +100,7 @@ ser.write(b'M105\r\n')
 ani = animation.FuncAnimation(fig, update, interval=int(timestep*1000), frames=int(max_timesteps/timestep), repeat=False)
 
 def save_fig(event):
+  global extrude_phase
   if event.key == 's':
     ani.event_source.stop()
     # fig.savefig('test.png')
@@ -83,6 +111,10 @@ def save_fig(event):
     np.save(f"{figname}_times", time_data)
 
     utils.close_printer(ser)
+  
+  if event.key == 'e':
+    extrude_phase += 1
+    print(f"Extrude phase: {extrude_phase}")
 
 cid = fig.canvas.mpl_connect('key_press_event', save_fig)
 
--- plot_printer_temp.py
+++ plot_printer_temp.py
@@ -1,5 +1,4 @@
 import time
-from cv2 import repeat
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
